f52160b38754c6f3e9509b8a4775099e0c9635d6
Balaji Srinivasan <93860462+pyCoder03@users.noreply.github.com>
2023-10-22 17:00:35 +0530

    Update test_activ.cpp

--- src/test_activ.cpp
+++ src/test_activ.cpp
@@ -1,8 +1,10 @@
-#include "activation.h"
+#include "../lib/activation.h"
 #include<iostream>
 using namespace std;
 int main()
 {
-    cout<<Activation::Sigmoid::function(3)<<"\n";
+    cout<<Activation::Sigmoid::function(10)<<"\n";
+    cout<<Activation::Sigmoid::derivative(0.5)<<"\n";
+    cout<<Activation::Sigmoid::derivative(1.25)<<"\n";
     return 0;
 }
