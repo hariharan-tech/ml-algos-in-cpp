16d6b015285987ee3cf72393fb64f3681e6a4772
Balaji Srinivasan <93860462+pyCoder03@users.noreply.github.com>
2023-10-22 17:03:49 +0530

    Update Regression.h

--- lib/Regression.h
+++ lib/Regression.h
@@ -8,21 +8,38 @@
 #define _STATS_H
 #endif
 
+#ifndef _DATAFRAME_H
+#include "dataframe.h"
+#define _DATAFRAME_H
+#endif
+
+typedef std::vector<std::vector<double>> vvd;
+
+double Identity_der(double);
+double Identity(double);
+
 class Regression{
     std::vector<double> weights;
     double bias;
-    double (*activation)(double);
-    double (*activation_der)(double,bool);
+    // double (*activation)(double);
+    double (*activation_der)(double)=Identity_der;           // Derivative of the activation function
+    double (*activation_func)(double)=Identity;
     bool is_batch;
-    void Back_propagate_batch(unsigned int);
-    void Back_propagate_online(unsigned int); 
+    void export_weights(std::ofstream*);
+    void Back_propagate_batch(unsigned int,std::ofstream*);     // History file
+    void Back_propagate_online(unsigned int,std::ofstream*);  
     public:
         int dim;
         double alpha;       // Learning rate
-        Regression(unsigned int);
-        Regression(unsigned int,std::vector<double>);
-        void fit(unsigned int);
-        void fit(bool,unsigned int);             // Batch/Online learning, Number of epochs
-        void fit(bool,std::string,unsigned int); // Extra is activation function
-        double test(std::vector<double>);
+        vvd data_table;
+        Regression(unsigned int);   // Dimension
+        Regression(std::vector<double>);   // Initial weights
+        Regression(unsigned int,double (*)(double),double (*)(double));
+        Regression(std::vector<double>,double (*)(double),double (*)(double));
+        void fit(unsigned int,double,Dataframe&);       // Double variable is the learning rate
+        void fit(bool,unsigned int,double,Dataframe&);  // Batch/Online learning, Number of epochs
+        void fit(bool,unsigned int,double,Dataframe&,std::ofstream*);   // For printing 
+        // void fit(bool,unsigned int); // Extra is activation function
+        double test(std::vector<double>::iterator,std::vector<double>::iterator);
+        double test(std::vector<double>::iterator,std::vector<double>::iterator,double);
 };
