c2c052215e7e0938cdfdd9525feeb5f7919f6418
Hariharan A <hariharanatech@gmail.com>
2024-02-02 23:10:44 +0530

    documented code & py test script

--- jacobi_svd/SVD.cpp
+++ jacobi_svd/SVD.cpp
@@ -1,14 +1,21 @@
+// This project takes in the 2 Dimension matrix in the form 1 D array.
+
 #include <iostream>
 #include <math.h>
 
-#define DIM 4
-#define PRECISION 0.5f
+#define DIM 5 // N X N Matrix dimension, this DIM is N
+#define PRECISION 0.5f // Off diagonal sum threshold for stopping iteration
 
+/*Matrix Multiplication of 2 matrices of dimension N X N (Where N is specified with template)
+  #### Inputs
+  - `a` and `b` are the input N x N matrix
+  #### Outputs
+  - `c` is the product N x N matrix
+*/
 template <int N>
 void matrix_mul(const float a[],const float b[], float c[])
 {
-   // Matrix multiplication of 2 NxN matrices 
-   float sum;
+   float sum; // Sum variable
    for (int i=0;i<N;i++)
       for (int j=0; j<N;j++)
       {
@@ -22,28 +29,37 @@ void matrix_mul(const float a[],const float b[], float c[])
       
 }
 
-template<int N >
+/* Generates J (Jacobi matrix) and J' for a given
+  #### Inputs
+  - `i` and `j` are the values for i_th row and j_th column (indicates the location of maximum non diagonal element)
+  - `a` matrix is the input matrix
+  #### Outputs
+  - `rot_mat` is the Jacobi rotation matrix and its transpose is `rot_mat_t`
+*/
+template<int N>
 void rot_init(const int i, const int j, const float a[],float rot_mat[],float rot_mat_t[])
 { 
-   // To calculate the rotation Matrix J and J transpose
-   float aii, aij;
+   float aii, aij; // variable to store cos(theta) and sin(theta)
    float theta;
-   theta=0.5*atan((2*a[i*N+j])/(a[i*N+i]-a[j*N+j]));
+   theta=0.5*atan((2*a[i*N+j])/(a[i*N+i]-a[j*N+j])); // Calculating theta using (2aij/(aii-ajj))
    aii= cos(theta);
    aij= sin(theta);
 
+  // Creating Identity matrix for J and J'
    for(int r=0; r<N ;r++)
      for(int c=0;c<N; c++)
      {
        rot_mat[r*N+c] = c==r?1:0 ; 
        rot_mat_t[r*N+c] = c==r?1:0 ; 
      }
-    
+  
+  // Writing sin(theta) and cos(theta) at respective places
+  // For matrix J
    rot_mat[i*N+i]=aii;
    rot_mat[i*N+j]=aij;
    rot_mat[j*N+i]=-aij;
    rot_mat[j*N+j]=aii;
-
+  // For matrix J'
    rot_mat_t[i*N+i]=aii;
    rot_mat_t[i*N+j]=-aij;
    rot_mat_t[j*N+i]=aij;
@@ -51,6 +67,12 @@ void rot_init(const int i, const int j, const float a[],float rot_mat[],float ro
 
 }
 
+/*Find the row and column of the absolute maximum non-diagonal value in the matrix
+  #### Inputs
+  - `input_mat` is the 1D N*N matrix
+  #### Outputs
+  - `r` and `c` are the row and column value respectively
+*/
 template <int N>
 void non_diag_max(float const input_mat[], int *r, int *c)
 {
@@ -66,6 +88,12 @@ void non_diag_max(float const input_mat[], int *r, int *c)
     *r = max_r; *c = max_c;
 }
 
+/*Find the sum of absolute of non-diagonal elements in a matrix
+  #### Inputs
+  - `input_mat` is the 1D N*N matrix
+  #### Outputs
+  - `sum` is the absolute sum
+*/
 template<int N>
 void non_diag_sum(float const input_mat[], float *sum){
   float temp=0;
@@ -79,41 +107,53 @@ void non_diag_sum(float const input_mat[], float *sum){
 
 int main()
 {
-  float a[DIM*DIM]= {9,2,3,4,2,8,6,7,3,6,16,9,4,7,9,10};
+  // Declare variables (and initialize some)
+  float a[DIM*DIM]= { 10, 2, 3, 4, 5,
+                      2, 6, 7, 8, 2,
+                      3, 7, 10, 11, 4,
+                      4, 8, 11, 10, 1,
+                      5, 2, 4, 1, 18};
   float rot_matrix[DIM*DIM],rot_matrix_t[DIM*DIM];
   float temp[DIM*DIM],eigvec[DIM*DIM];
 
-  //Initialize Eigen Vector matrix
+  //Initialize Eigen Vector matrix (as Identity)
   for(int r=0; r<DIM ;r++)
    for(int c=0;c<DIM; c++)
        eigvec[r*DIM+c] = c==r?1:0 ;
 
   int row,col;
   float prec;
-  non_diag_sum<DIM>(a,&prec);
-  for(int i=0;prec>PRECISION;i++)
-  {
-      non_diag_max<DIM>(a,&row,&col);
-      rot_init<DIM>(row,col,a,rot_matrix,rot_matrix_t);
-      matrix_mul<DIM>(rot_matrix,a,temp);
-      matrix_mul<DIM>(temp,rot_matrix_t,a);
 
-      matrix_mul<DIM>(rot_matrix,eigvec,temp);
+  // Find the initial non-diagonal values absolute sum
+  non_diag_sum<DIM>(a,&prec);
+
+  // Iterate until the precision is greater the set Threshold
+  for(int i=0;prec>PRECISION;i++)
+  {   
+      non_diag_max<DIM>(a,&row,&col); // Find the location of non-diagonal absolute max value's row and col
+      rot_init<DIM>(row,col,a,rot_matrix,rot_matrix_t); // Create the required J and J' matrix with row and col
+      matrix_mul<DIM>(rot_matrix,a,temp); // Matrix multiply J and A
+      matrix_mul<DIM>(temp,rot_matrix_t,a); // Computing JAJ' now
+
+      // The final eigenvector is a product of rotation matrix at each iteration
+      matrix_mul<DIM>(rot_matrix,eigvec,temp); // Multiplying Ji with J to find eigenvector
 
       //Reassign Eigen vector Matrix
-      
       for (int index=0;index < DIM*DIM; index++)
           eigvec[index]=temp[index];
 
+      // Find the non-diagonal absolute sum
       non_diag_sum<DIM>(a,&prec);
       std::cout<<i<<"\t"<<prec<<"\t\t\t\t";
       
+      // Printing eigenvalues for each iteration
       for (int k=0;k<DIM;k++)
       {
           std::cout<<a[k*DIM+k]<<" ";
       }
       std::cout<<std::endl;
   }
+  // Printing final eigenvectors
   std::cout<<std::endl;
   for (int i=0; i<DIM; i++)
   {
--- jacobi_svd/test.py
+++ jacobi_svd/test.py
@@ -2,6 +2,10 @@ import sys
 import ctypes
 import numpy as np
 
-a = np.array(eval(sys.argv[1]),dtype=np.float32)
+a = np.array([1, 2, 3, 4, 5,
+            2, 6, 7, 8, 9,
+            3, 7, 10, 11, 12,
+            4, 8, 11, 13, 14,
+            5,9,12,14,15]).reshape((5,5))
 eigval, eigvec = np.linalg.eig(a);
-print(f"Eigenvalues {eigval}\n\n Eigenvector:\n{eigvec[:,0]}\n{eigvec[:,1]}")
+print(f"Eigenvalues {eigval}\n\n Eigenvector:\n{eigvec}")
