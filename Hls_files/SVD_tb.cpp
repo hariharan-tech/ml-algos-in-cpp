bc474a40d7237ac03c13b42f111e4e94a8e7da66
Hariharan Ananth <72250173+hariharan-tech@users.noreply.github.com>
2024-02-05 11:20:47 +0530

    Update SVD_tb.cpp

--- Hls_files/SVD_tb.cpp
+++ Hls_files/SVD_tb.cpp
@@ -1,23 +1,23 @@
-#include "SVD_top.cpp"
 #include<iostream>
+#include "SVD_top.h"
 
-#define DIM 4
+#define DIMEN 4
 
 int main(void)
 {
-  float a[DIM*DIM]= { 10, 2, 3, 4,
+  float a[DIMEN*DIMEN]= { 10, 2, 3, 4,
                       2, 6, 7, 8,
                       3, 7, 10, 11,
-                      4, 8, 11, 10,};
+                      4, 8, 11, 10};
 
-  float eig_vector[DIM*DIM];
+  float eig_vector[DIMEN*DIMEN], eig_value[DIMEN*DIMEN];
 
-  svd(a,eig_vector);
+  svd(a,eig_value,eig_vector);
 
-  for (int i=0; i<DIM; i++)
+  for (int i=0; i<DIMEN; i++)
   {
-    for (int j=0;j<DIM;j++)
-      std::cout<<a[i*DIM+j]<<" ";
+    for (int j=0;j<DIMEN;j++)
+      std::cout<<eig_value[i*DIMEN+j]<<" ";
     std::cout<<std::endl;
   }
 }
